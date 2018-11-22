96a8e802bbe9d8a00e09512c048ba614bb749206
adcrisostomo <adcrisostomo2@up.edu.ph>
2018-11-23 03:37:32 +0800

    Added error handling, disconnection handling, and made other minor improvements

--- test.py
+++ test.py
@@ -1,17 +1,30 @@
+"""
+CMSC 137 CD-5L Final Project: Milestone #2
+Group: Quartet Mirana
+    Crisostomo, Albert Dominic
+    Fortes, Patricia
+    Mojar, Kenneth
+    Umali, Harold
+"""
 import player_pb2 as PlayerModule
 import tcp_packet_pb2 as TcpPacketModule
 import socket as Socket
 import select
 import sys
 
+# constants
 BUFFER_SIZE = 1024
+SUCCESSFUL = 0
+UNSUCCESSFUL = 1
+LOBBY_DNE = 2
+LOBBY_FULL = 3
 
 # global client socket
 server_address = ("202.92.144.45", 80)
 client_socket = Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
-def createLobby(max_players=4):
+def createLobby(player_name, max_players=4):
     # instantiate attributes
     create_lobby_packet = TcpPacketModule.TcpPacket.CreateLobbyPacket()
     create_lobby_packet.type = TcpPacketModule.TcpPacket.CREATE_LOBBY
@@ -20,37 +33,50 @@ def createLobby(max_players=4):
     # send then receive
     client_socket.sendall(create_lobby_packet.SerializeToString())
     data = client_socket.recv(BUFFER_SIZE)
+
     # parse received data
     create_lobby_packet = TcpPacketModule.TcpPacket.CreateLobbyPacket()
     create_lobby_packet.ParseFromString(data)
     
     # debug
-    print( "Lobby created! Your lobby ID is \"{}\"".format(create_lobby_packet.lobby_id) )
-    print( "Auto joining lobby..." )
+    # print(create_lobby_packet)
 
-    #auto join creator
-    joinLobby(create_lobby_packet.lobby_id)
+    # auto join creator
+    joinLobby(create_lobby_packet.lobby_id, player_name)
 
-def joinLobby(lobby_id):
-    # instantiate player
-    player = PlayerModule.Player()
-    player.name = input("Enter name: ")
+    return create_lobby_packet.lobby_id
+
+def joinLobby(lobby_id, player_name):
+    tcp_packet = TcpPacketModule.TcpPacket()
 
     # instantiate attributes
     connect_packet = TcpPacketModule.TcpPacket.ConnectPacket()
     connect_packet.type = TcpPacketModule.TcpPacket.CONNECT
-    connect_packet.player.name = player.name
+    connect_packet.player.name = player_name
     connect_packet.lobby_id = lobby_id
     
+    # debugging purposes
+    # print(connect_packet)
+
     # send then receive
     client_socket.sendall(connect_packet.SerializeToString())
     data = client_socket.recv(BUFFER_SIZE)
-    #parse received data
-    connect_packet = TcpPacketModule.TcpPacket.ConnectPacket()
-    connect_packet.ParseFromString(data)
 
-    # debug
-    # print(connect_packet)
+    # parse received data
+    tcp_packet.ParseFromString(data)
+
+    # debugging purposes
+    # print(tcp_packet)
+
+    if tcp_packet.type == TcpPacketModule.TcpPacket.ERR_LDNE:
+        return LOBBY_DNE
+    elif tcp_packet.type == TcpPacketModule.TcpPacket.ERR_LFULL:
+        return LOBBY_FULL
+    elif tcp_packet.type == TcpPacketModule.TcpPacket.ERR:
+        return UNSUCCESSFUL
+    elif tcp_packet.type == TcpPacketModule.TcpPacket.CONNECT:
+        return SUCCESSFUL
+
 
 def send(message):
     # instantiate attributes
@@ -58,12 +84,12 @@ def send(message):
     chat_packet.type = TcpPacketModule.TcpPacket.CHAT
     chat_packet.message = message
 
+    # client exits
     if chat_packet.message == "/exit\n":
         quitLobby()
-    elif chat_packet.message == "/show_players\n":
+    # client requests list of players currently in lobby
+    elif chat_packet.message == "/players\n":
         showAllPlayers()
-    elif chat_packet.message == "\n":
-        return
     else:
         client_socket.sendall(chat_packet.SerializeToString())
 
@@ -74,28 +100,41 @@ def receive(socks):
     data = socks.recv(BUFFER_SIZE)
     tcp_packet.ParseFromString(data)
 
-    # receive message
+    # received message
     if tcp_packet.type == TcpPacketModule.TcpPacket.CHAT:
+        # create packet holder
         chat_packet = TcpPacketModule.TcpPacket.ChatPacket()
         chat_packet.ParseFromString(data)
 
+        # write message
         sys.stdout.write("<"+chat_packet.player.name+"> ")
         sys.stdout.write(chat_packet.message)
-        sys.stdout.flush() # display written message
-    # receive disconnect packet
+        # display written message
+        sys.stdout.flush() 
+
+    # received disconnect packet
     elif tcp_packet.type == TcpPacketModule.TcpPacket.DISCONNECT:
+        # create packet holder
         disconnect_packet = TcpPacketModule.TcpPacket.DisconnectPacket()
         disconnect_packet.ParseFromString(data)
 
-        sys.stdout.write(disconnect_packet.player.name+" has disconnected from the chat room.\n")
-        sys.stdout.flush() # display written message
-    # receive connect packet
+        # check packet type
+        if disconnect_packet.update == TcpPacketModule.TcpPacket.DisconnectPacket.NORMAL:
+            sys.stdout.write(disconnect_packet.player.name+" has disconnected from the chat lobby.\n")
+        elif disconnect_packet.update == TcpPacketModule.TcpPacket.DisconnectPacket.LOST:
+            sys.stdout.write(disconnect_packet.player.name+" lost connection from the chat lobby.\n")
+
+        # display server message
+        sys.stdout.flush() 
+
+    # received connect packet
     elif tcp_packet.type == TcpPacketModule.TcpPacket.CONNECT:
+        # create packet holder
         connect_packet = TcpPacketModule.TcpPacket.ConnectPacket()
         connect_packet.ParseFromString(data)
 
-        sys.stdout.write(connect_packet.player.name+" has connected to the chat room.\n")
-        sys.stdout.flush() # display written message
+        sys.stdout.write(connect_packet.player.name+" has connected to the chat lobby.\n")
+        sys.stdout.flush()
 
 def quitLobby():
     # instantiate attributes
@@ -113,10 +152,17 @@ def showAllPlayers():
     client_socket.sendall(player_list_packet.SerializeToString())
     data = client_socket.recv(BUFFER_SIZE)
     # parse received data
-    player_list_packet = TcpPacketModule.TcpPacket.PlayerListPacket()
     player_list_packet.ParseFromString(data)
-    print(player_list_packet)
 
+    # display players
+    sys.stdout.write("\nPlayers in Lobby\n")
+    sys.stdout.write("=====================\n")
+    for player in player_list_packet.player_list:
+        sys.stdout.write(player.name+"\n")
+    sys.stdout.write("=====================\n\n")
+    sys.stdout.flush()
+
+player_name = input("Enter your name: ")
 choice = 0
 message = ""
 
@@ -128,26 +174,39 @@ while choice != "3":
     choice = input(">> ")
 
     if choice == "1":
-        createLobby()
+        lobby_id = createLobby(player_name)
+        print("Welcome to the chat lobby, {}! The Lobby ID is {}.".format(player_name, lobby_id))
     elif choice == "2":
         lobby_id = input("Enter Lobby ID: ")
-        joinLobby(lobby_id)
+        result = joinLobby(lobby_id, player_name)
+        if result == LOBBY_DNE:
+            print("Joining unsuccessful. The chat lobby does not exist.")
+            continue
+        elif result == LOBBY_FULL:
+            print("Joining unsuccessful. The chat lobby is already full.")
+            continue
+        elif result == UNSUCCESSFUL:
+            print("Joining unsuccessful.")
+            continue
+        else:
+            print("Welcome to the chat lobby, {}!".format(player_name))
     elif choice == "3":
         break
     else:
         print("Invalid input")
-        continue
 
 
     while message != "/exit\n":
-        # maintains a list of possible input streams 
         sockets_list = [sys.stdin, client_socket] 
 
         read_sockets, write_socket, error_socket = select.select(sockets_list,[],[]) 
         
-        for socks in read_sockets: 
+        # check each socket to determine source of data input
+        for socks in read_sockets:
+            # input came from server
             if socks == client_socket: 
                 receive(socks)
+            # input came from client
             else:
                 message = sys.stdin.readline()
                 send(message)
