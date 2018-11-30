fa0a8843dbdb919531225b782cbefb97f057a66b
mikenmo <mdmojar@up.edu.ph>
2018-11-30 16:49:35 +0800

    Added arrow object and arrow upgrade

--- src/classes/Arrow.py
+++ src/classes/Arrow.py
@@ -1,7 +1,17 @@
+from classes.Player import *
 class Arrow:
-    def __init__(self,ownerId,xpos,ypos):
-        self.ownerId = ownerId
+    def __init__(self,playerId,xpos,ypos,angle, power, distance, speed):
+        self.playerId = playerId
         self.xpos = xpos
+        self.startx = xpos
         self.ypos = ypos
+        self.starty = ypos
+        self.angle = angle
+        self.power = power
+        self.distance = distance
+        self.speed = speed
         self.sprite = pygame.Surface((20,20))
         self.sprite.fill((255,0,0))
+    def move(self):
+        self.xpos += self.speed * 6 * math.cos(self.angle)
+        self.ypos += self.speed * 6 * math.sin(self.angle)
--- src/main.py
+++ src/main.py
@@ -33,21 +33,24 @@ x,y = w/2,h/2
 #     i+=1
 
 player_move = False
-# player_shoot = False
+player_shoot = False
 player_leap = False
 running = True
 i=0
+arrow = ''
 while running:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             raise SystemExit
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # if event.button == 1:
-            #     if not player_shoot:
-            #         target_x,target_y = player_x, player_y
-            #         player_shoot = True
-            #         shoot_mouse_x, shoot_mouse_y = pygame.mouse.get_pos()
+            if event.button == 1:
+                player_shoot = True
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                #compute the angle
+                dx = mouse_x - player.xpos
+                dy = mouse_y - player.ypos
+                arrow = Arrow(player.id,player.xpos,player.ypos,math.atan2(dy, dx),player.power,player.distance,player.speed)
             if event.button == 3:
                 player_move = True
                 mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -64,17 +67,26 @@ while running:
                 i=0
             elif event.key == pygame.K_s:
                 player_move = False
+            elif event.key == pygame.K_z:
+                player.power +=1
+            elif event.key == pygame.K_x:
+                player.distance +=1
+            elif event.key == pygame.K_c:
+                player.speed +=1
                 
     if player_move:
-        player.move(mouse_x, mouse_y)
+        player.move()
         if mouse_x-1.0 < player.xpos < mouse_x+1.0 or mouse_y-1.0 < player.ypos < mouse_y+1.0:
             player_move = False
             x,y = mouse_x, mouse_y
-#     if player_shoot:
-#         shoot(shoot_mouse_x,shoot_mouse_y)
-#         if -20 > bullet_x or bullet_x > w or -20 > bullet_y or bullet_y > h:
-#             bullet_x, bullet_y = player_x,player_y
-#             player_shoot = False
+    if player_shoot:
+        arrow.move()
+        if -20 > arrow.xpos or arrow.xpos > w or -20 > arrow.ypos or arrow.ypos > h:
+            bullet_x, bullet_y = player_x,player_y
+            player_shoot = False
+        if math.sqrt((arrow.xpos-arrow.startx)**2 + (arrow.ypos-arrow.starty)**2) > arrow.distance*100+500:
+            player_shoot = False
+            arrow = ''
     if player_leap:
         player.leap()
         if i==8:
@@ -82,5 +94,6 @@ while running:
         i+=1        
     screen.fill((0, 0, 0))
     screen.blit(player.sprite, (player.xpos-12.5, player.ypos-12.5))
-    # screen.blit(bullet, (bullet_x+2.5, bullet_y+2.5))s
+    if(arrow!=''):
+        screen.blit(arrow.sprite, (arrow.xpos+2.5, arrow.ypos+2.5))
     pygame.display.update()
